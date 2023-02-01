36e9f971218c6cd91575a1a2c2ec15716007225f
Anthony Salemo <ajssalemo@gmail.com>
2023-01-31 19:06:38 -0500

    testing with Kint

---
+++ kint/app.php
@@ -0,0 +1,9 @@
+<?php
+
+include 'vendor/autoload.php';
+
+$time = 1;
+$data = "data";
+$object = array("1", "2");
+ 
+d($time, $data, $object);
---
+++ kint/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'fef2571b71f7702624b30691bb08cb8ecb3c7e9e',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'fef2571b71f7702624b30691bb08cb8ecb3c7e9e',
+            'dev_requirement' => false,
+        ),
+        'kint-php/kint' => array(
+            'pretty_version' => '5.0.3',
+            'version' => '5.0.3.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../kint-php/kint',
+            'aliases' => array(),
+            'reference' => '03169f1923966c815223adbb3eccf831a3423578',
+            'dev_requirement' => false,
+        ),
+    ),
+);
